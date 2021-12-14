7ed99422191b0bf24d5b9b5f6f6a9f24de8b4eb7
maryemsalah22 <maryemsalah61@gmail.com>
2021-12-14 17:52:14 +0200

    Fetch stage is ready for one operand without any hazards handling

---
+++ fetch/InstructionMemory.vhd
@@ -0,0 +1,22 @@
+Library ieee;
+use ieee.std_logic_1164.all;
+use  IEEE.numeric_std.all;
+
+Entity InstMEM is 
+port(                               
+clk : in std_logic;               
+PC : in std_logic_vector(31 downto 0);   
+FetchedInstruction : out std_logic_vector(15 downto 0));
+end InstMEM ;
+
+architecture IMArc of InstMEM is
+TYPE ram_type IS ARRAY(0 TO 1048575) of std_logic_vector(15 DOWNTO 0);
+SIGNAL ram : ram_type ;
+begin
+ process(clk)
+  begin                                                                              
+    if(rising_edge(clk) and to_integer(unsigned(PC)) < 1048576 )then 
+      FetchedInstruction<=ram(to_integer(unsigned(PC)));
+    end if;     
+  end process ;
+end IMArc ;
---
+++ fetch/PC_Reg.vhd
@@ -0,0 +1,22 @@
+Library ieee;
+use ieee.std_logic_1164.all;
+use  IEEE.numeric_std.all;
+------------------PC Register-------------------------
+Entity PCounter is 
+port(
+DataIn : in std_logic_vector(31 downto 0);
+clk,reset : in std_logic ;
+DataOut : out std_logic_vector(31 downto 0));
+end PCounter ;
+
+architecture PCounterArc of PCounter is
+begin
+process(clk,reset)
+begin
+if(reset='1' and rising_edge(clk)) then
+  DataOut<=std_logic_vector(to_unsigned(0,32));
+elsif(rising_edge(clk)) then
+   DataOut<=DataIn;
+end if;
+end process ;
+end PCounterArc ;
